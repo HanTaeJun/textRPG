7e69cfb72e37618bc446d2b37c105d1f093bebda
HTJ <ac85kr@gmail.com>
2019-03-11 15:50:53 +0900

    commit

---
+++ src/Views/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+          ㅎㅇㅎㅇ
+      </div>
+    );
+  }
+}
+
+export default Home;
