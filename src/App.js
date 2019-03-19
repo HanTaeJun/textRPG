2049efc57f6c645f8f532275f5511d0b38111037
unknown <ac85kr@gmail.com>
2019-03-19 15:39:12 +0900

    commit data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import './App.css';
 
-import {Home} from "./Views/home";
+import {Home} from "Views/home";
+import {About} from "Views/about";
+import {Menu} from "Views/menu";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
+        <Menu/>
         <Route exact path="/" component={Home}/>
-        <Route exact path="/home" component={Home}/>
-      </div>
+        <Switch>
+          <Route path="/about/:name" component={About}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+      </>
     );
   }
 }
